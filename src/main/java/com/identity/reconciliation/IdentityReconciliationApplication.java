f2401d77480adc4f809a629120b8208f885fe921
Aman Pathak <amanpathak@Aman.local>
2024-06-01 20:56:17 +0530

    Identity reconciliation project initial commit

--- src/main/java/com/identity/reconciliation/IdentityReconciliationApplication.java
+++ src/main/java/com/identity/reconciliation/IdentityReconciliationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IdentityReconciliationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IdentityReconciliationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityReconciliationApplication.class, args);
+    }
 
 }
